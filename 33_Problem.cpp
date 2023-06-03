61aac1b547bbf15542f75d30e8fdde20bb70d89b
Aniruddh Joshi <118598273+aniruddh-joshi@users.noreply.github.com>
2023-06-03 04:48:20 +0000

    Task Selection

--- 33_Problem.cpp
+++ 33_Problem.cpp
@@ -1,25 +1,46 @@
 #include <iostream>
 #include <vector>
+#include <algorithm>
 using namespace std;
 
+struct Task {
+    int timeTaken;
+    int deadline;
+    int index;
+
+    Task(int t, int d, int i) : timeTaken(t), deadline(d), index(i) {}
+};
+
+bool compareTasks(const Task& task1, const Task& task2) {
+    return task1.deadline < task2.deadline;
+}
+
 void selectTasks(int n, vector<int>& timeTaken, vector<int>& deadlines) {
-    int count = 0;
+    // Create a vector of Task objects
+    vector<Task> tasks;
+    for (int i = 0; i < n; i++) {
+        tasks.emplace_back(timeTaken[i], deadlines[i], i + 1);
+    }
+
+    // Sort the tasks based on the deadlines
+    sort(tasks.begin(), tasks.end(), compareTasks);
+
+    // Select the tasks based on the deadlines
     vector<int> selectedTasks;
     int totalTime = 0;
-
-    for (int i = 0; i < n; i++) {
-        if (totalTime + timeTaken[i] <= deadlines[i]) {
-            count++;
-            selectedTasks.push_back(i + 1);
-            totalTime += timeTaken[i];
+    for (const Task& task : tasks) {
+        if (totalTime + task.timeTaken <= task.deadline) {
+            selectedTasks.push_back(task.index);
+            totalTime += task.timeTaken;
         }
     }
 
-    cout << "Max number of tasks = " << count << endl;
+    // Print the result
+    cout << "Max number of tasks = " << selectedTasks.size() << endl;
     cout << "Selected task numbers: ";
-    for (int i = 0; i < count; i++) {
+    for (int i = 0; i < selectedTasks.size(); i++) {
         cout << selectedTasks[i];
-        if (i != count - 1)
+        if (i != selectedTasks.size() - 1)
             cout << ", ";
     }
     cout << endl;
